a9a56622876b895516cdc14e4d4baa16e296d00d
yenpu <chen.yenpu@gmail.com>
2023-03-10 17:01:10 +0800

    Add parameter sleep

--- src/main/java/com/example/webflux/account/AccountHandler.java
+++ src/main/java/com/example/webflux/account/AccountHandler.java
@@ -9,6 +9,9 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+import java.util.Optional;
+
 
 @Component
 public class AccountHandler {
@@ -22,9 +25,13 @@ public class AccountHandler {
     }
 
     public Mono<ServerResponse> findAll(final ServerRequest request) {
+        final String sleepParam = request.queryParam("sleep").orElse("0");
+        final Integer sleep = Integer.valueOf(sleepParam);
+
         final PageRequest pageRequest = PageRequest.of(0, 100);
         final Mono<Page<Account>> mono = this.paginationRepository.findAllBy(pageRequest)
                 .collectList()
+                .delaySubscription(Duration.ofSeconds(sleep))
                 .zipWith(this.paginationRepository.count())
                 .map(t -> new PageImpl<>(t.getT1(), pageRequest, t.getT2()));
         return ServerResponse.ok()
